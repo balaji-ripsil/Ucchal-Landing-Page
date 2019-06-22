bbf8d48cd1f3c5e03b80eb2a08ba87a4a14be1d2
balaji-ripsil <ripsildeveloper5@gmail.com>
2019-06-22 14:03:09 +0530

    changes in home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,13 @@ import {ContentComponent} from '../home/content/content.component';
 import {VideoComponent} from '../home/video/video.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { from } from 'rxjs';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [ContentComponent, VideoComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
